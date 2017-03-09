674a5130c507f529a82c74f8453572a3d08e9b36
Daniel Quach <daniel@quach.info>
2017-03-09 10:39:42 +0100

    rendertarget merge

--- include/Vulkan/VulkanRenderTarget.hpp
+++ include/Vulkan/VulkanRenderTarget.hpp
@@ -14,23 +14,7 @@ namespace Eternal
 		class VulkanRenderTarget : public VulkanResource
 		{
 		public:
-<<<<<<< Updated upstream
-			//VulkanRenderTarget(_In_ VulkanDevice& Device);
-			VulkanRenderTarget(_In_ VkSurfaceKHR_T*& RenderTargetHandle);
-
-			VkSurfaceKHR_T*& GetRenderTarget() { return _RenderTarget; }
-
-		private:
-<<<<<<< HEAD
-			VkSurfaceKHR _RenderTarget;
-=======
 			VulkanRenderTarget();
-
-		private:
->>>>>>> Stashed changes
-=======
-			VkSurfaceKHR_T* _RenderTarget;
->>>>>>> 4a6e8cf3caa9bc469d4cbe28b1c95167a2572cce
 		};
 	}
 }
--- src/Vulkan/VulkanRenderTarget.cpp
+++ src/Vulkan/VulkanRenderTarget.cpp
@@ -4,16 +4,7 @@
 
 using namespace Eternal::Graphics;
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-VulkanRenderTarget::VulkanRenderTarget(_In_ VkSurfaceKHR& RenderTargetHandle)
-=======
-VulkanRenderTarget::VulkanRenderTarget(_In_ VkSurfaceKHR_T*& RenderTargetHandle)
->>>>>>> 4a6e8cf3caa9bc469d4cbe28b1c95167a2572cce
-	: _RenderTarget(RenderTargetHandle)
-=======
 VulkanRenderTarget::VulkanRenderTarget()
->>>>>>> Stashed changes
 {
 	//Device.GetDevice()
 
