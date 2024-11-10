5363ce91f4e5749037e853ef8ac42364cf116b2d
Daniel Quach <daniel@quach.info>
2024-11-10 19:34:45 +0000

    Generic shader

--- include/Graphics/Shader.hpp
+++ include/Graphics/Shader.hpp
@@ -21,6 +21,11 @@ namespace Eternal
 		{
 			D3D12_SHADER_COMPILER_TYPE_FXC = 0,
 			D3D12_SHADER_COMPILER_TYPE_DXC
+#if ETERNAL_PLATFORM_WINDOWS
+			, D3D12_SHADER_COMPILER_TYPE_DEFAULT = D3D12_SHADER_COMPILER_TYPE_FXC
+#else
+			, D3D12_SHADER_COMPILER_TYPE_DEFAULT = D3D12_SHADER_COMPILER_TYPE_DXC
+#endif
 		};
 
 		struct ShaderCreateInformation
@@ -28,7 +33,7 @@ namespace Eternal
 			ShaderCreateInformation(_In_ ShaderType InStage, _In_ const char* InName, _In_ const char* InFileName, _In_ const vector<string>& InDefines = vector<string>());
 
 			ShaderType Stage								= ShaderType::SHADER_TYPE_COUNT;
-			D3D12ShaderCompilerType D3D12CompilerPreference	= D3D12ShaderCompilerType::D3D12_SHADER_COMPILER_TYPE_FXC;
+			D3D12ShaderCompilerType D3D12CompilerPreference	= D3D12ShaderCompilerType::D3D12_SHADER_COMPILER_TYPE_DEFAULT;
 			string Name;
 			string FileName;
 			vector<string> Defines;
