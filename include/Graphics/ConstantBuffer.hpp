b9903899fb9d38769dfe27041e71e92655a499ce
Daniel Quach <daniel@quach.info>
2025-05-18 18:05:26 +0100

    Fixed constant buffer and additional helper

--- include/Graphics/ConstantBuffer.hpp
+++ include/Graphics/ConstantBuffer.hpp
@@ -58,8 +58,97 @@ namespace Eternal
 				return *ResourceBuffer;
 			}
 
+			operator View* ()
+			{
+				return *ResourceView;
+			}
+
+			operator View& ()
+			{
+				return *this;
+			}
+
+			operator Resource* ()
+			{
+				return *ResourceBuffer;
+			}
+
+			operator Resource& ()
+			{
+				return *this;
+			}
+
 			MultiBuffered<Resource>*	ResourceBuffer	= nullptr;
 			MultiBuffered<View>*		ResourceView	= nullptr;
+		};
+
+		template<typename ConstantBufferType>
+		struct FixedConstantBuffer
+		{
+			FixedConstantBuffer(_In_ GraphicsContext& InContext, _In_ const char* InBufferName)
+			{
+				ResourceBuffer = CreateBuffer(
+					BufferResourceCreateInformation(
+						InContext.GetDevice(),
+						InBufferName,
+						BufferCreateInformation(
+							Format::FORMAT_UNKNOWN,
+							BufferResourceUsage::BUFFER_RESOURCE_USAGE_CONSTANT_BUFFER,
+							sizeof(ConstantBufferType)
+						),
+						ResourceMemoryType::RESOURCE_MEMORY_TYPE_GPU_UPLOAD
+					)
+				);
+
+				ViewMetaData MetaData;
+				MetaData.ConstantBufferView.BufferSize = sizeof(ConstantBufferType);
+				ResourceView = CreateConstantBufferView(
+					ConstantBufferViewCreateInformation(
+						InContext,
+						ResourceBuffer,
+						MetaData
+					)
+				);
+			}
+
+			~FixedConstantBuffer()
+			{
+				DestroyResource(ResourceBuffer);
+				DestroyView(ResourceView);
+			}
+
+			View* GetView()
+			{
+				return ResourceView;
+			}
+
+			Resource* GetResource()
+			{
+				return ResourceBuffer;
+			}
+
+			operator View* ()
+			{
+				return ResourceView;
+			}
+
+			operator View& ()
+			{
+				return *ResourceView;
+			}
+
+			operator Resource* ()
+			{
+				return ResourceBuffer;
+			}
+
+			operator Resource& ()
+			{
+				return *ResourceBuffer;
+			}
+
+			Resource*	ResourceBuffer	= nullptr;
+			View*		ResourceView	= nullptr;
 		};
 	}
 }
